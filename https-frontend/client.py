cd12f318131b49565c75853ce9180ea12f235215
scott coulton <scott.coulton@microsoft.com>
2021-12-09 21:25:31 +0000

    changed streaming to do 100 requests

--- https-frontend/client.py
+++ https-frontend/client.py
@@ -24,8 +24,11 @@ greeter = greet_pb2_grpc.GreeterStub(channel)
 
 @app.route('/hello', methods=['GET'])
 def getHello():
-    helloRequest = greet_pb2.HelloRequest(name='Azure Container Apps')
-    response = greeter.SayHello(helloRequest, timeout=25)
-    return response.message
+    req = 100
+    for x in range(req):
+      myhost = os.uname()[1]
+      helloRequest = greet_pb2.HelloRequest(name='%s' % myhost) 
+      response = greeter.SayHello(helloRequest, timeout=25)
+      return response.message
 
 app.run(host='0.0.0.0', port=os.getenv('PORT', '8050'))
